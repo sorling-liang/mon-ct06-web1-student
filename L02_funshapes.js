f93dbbd4e915b7faede47a6af7a0a6683a8e43a9
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-07-14 10:00:57 +0000

    Jul 14, 2025, 10:00 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -4,7 +4,7 @@ Task 1: Add the setup() function
 function setup() {
   // run once
   createCanvas(400, 400); // a blank drawing canvas
-  background(100);
+  background(220);
 }
 
 // forever block
