6b75d7a304caeb9d5901b3cc094538f12deebf34
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-07-14 10:09:47 +0000

    Jul 14, 2025, 10:09 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -13,7 +13,7 @@ function draw() {
   // Challenge 1: An eye using ellipse() function
 
   // Challenge 2: A face using circle() function
-  
+  //      x    y   diameter
   circle(150, 150, 100);
   // Challenge 3: A house using rect() function
 
