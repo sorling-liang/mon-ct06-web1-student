23bbb480070c5c72160259a91cc791350a3f4474
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-07-14 10:31:25 +0000

    Jul 14, 2025, 10:31 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -20,7 +20,7 @@ function draw() {
   // rect(35, 150, 300, 200);
 
   // Challenge 4: A rocketship using triangle() function
-  triangle(50,50, 150,150, 300, 200);
+  triangle(50,50, 150,150, 350, 200);
 
   // Challenge 5: A square face using quad() function
 
