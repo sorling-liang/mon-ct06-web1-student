4efcc6754c7051c2c4d749dc74487b81755f2be7
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-07-28 10:39:42 +0000

    Jul 28, 2025, 10:39 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -26,7 +26,7 @@ function draw() {
 
     // background(220); // erase the background
     noStroke();
-    fill(5, 50);
+    fill(5, 50); // gray-value, alpha
     circle(mouseX, mouseY, 50);
 
 } // this one must be the last line
