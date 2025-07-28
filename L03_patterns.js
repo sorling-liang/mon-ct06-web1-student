9d294a50b5449932eccbc0304c2c0ff3a0b482af
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-07-28 09:43:18 +0000

    Jul 28, 2025, 9:43 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -44,11 +44,11 @@ function draw() {
 
   // repeat 5 times
   for (let i=0; i<5; i=i+1) {
-    if (i%2==0)
+    if (i%2 == 0)
       fill("black");
     else
       fill("white");
-    
+
     rect(xpos, ypos, 50, 50);
     xpos = xpos + 60;
   }
