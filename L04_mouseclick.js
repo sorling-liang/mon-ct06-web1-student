353f269f0d7a175da9609552530aa0633e78bf0c
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-07-28 10:33:34 +0000

    Jul 28, 2025, 10:33 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -8,18 +8,22 @@ function setup() {
 
 // forever block
 function draw() {
-    let r = 0; // red value
-    let g = 0; // green value
-    let b = 0; // blue value
-    let xpos = 50;
-    let ypos = 50;
+    
+    // let r = 0; // red value
+    // let g = 0; // green value
+    // let b = 0; // blue value
+    // let xpos = 50;
+    // let ypos = 50;
 
-    // put your code inside here
-    for ( let i=0; i<6; i++) {
-        fill(r, g, b);
-        g = g + 50; // increase green to more
-        circle(xpos, ypos, 50);
-        xpos = xpos + 50; // move right
-        ypos = ypos + 50; // move down
-    }
+    // // put your code inside here
+    // for ( let i=0; i<6; i++) {
+    //     fill(r, g, b);
+    //     g = g + 50; // increase green to more
+    //     circle(xpos, ypos, 50);
+    //     xpos = xpos + 50; // move right
+    //     ypos = ypos + 50; // move down
+    // }
+
+
+
 } // this one must be the last line
