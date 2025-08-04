9444168e6908a4fb8c71b528aafc4c4e21003e9d
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-04 09:55:09 +0000

    Aug 4, 2025, 9:55 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -3,7 +3,7 @@
 // run once
 function setup() {
     createCanvas(600, 400);
-    background(200); // valid value 0-255
+    background(80); // valid value 0-255
 }
 
 // repeatedly 60 times per second
