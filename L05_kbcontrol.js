1629d7202fb80c5a1b2e6af52cc1d12f65c40a16
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-11 10:34:00 +0000

    Aug 11, 2025, 10:34 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -17,9 +17,9 @@ function draw() {
     if ( keyIsDown(LEFT_ARROW) ) {
         xpos = xpos - 5;
     }
-    
+
     if ( keyIsDown(RIGHT_ARROW) ) {
-        xpos = xpos + 5;
+        ypos = ypos + 5;
     }
     if ( keyIsDown(LEFT_ARROW) ) {
         xpos = xpos - 5;
