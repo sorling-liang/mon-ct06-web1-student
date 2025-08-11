fa546c9a34fbe7841a1eff790778938a30e668ed
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-11 13:14:23 +0000

    Aug 11, 2025, 1:14 PM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -10,49 +10,30 @@ function setup() {
 // repeatedly 60 times per second
 function draw() {
     background(220); // clear the drawing paper
-
-    if ( keyIsDown(RIGHT_ARROW) ) {
-        xpos = xpos + 5;
-    }
-    if ( keyIsDown(LEFT_ARROW) ) {
-        xpos = xpos - 5;
-    }
-    xpos = constrain(xpos, 0+25, width-25);
-
-    if ( keyIsDown(UP_ARROW) ) {
-        ypos = ypos - 5;
-    }
-    if ( keyIsDown(DOWN_ARROW) ) {
-        ypos = ypos + 5;
-    }    
-    ypos = constrain(ypos, 0+25, height-25);
-
-    fill("yellow");
-    circle( xpos, ypos, 50 ); // player
 }
 
-// function keyPressed() {
-//     // background(220);
-//     // textSize(64); // font size
-//     // //   text  x   y
-//     // text(key, 50, 50);
-//     // text(keyCode, 50, 120);
+function keyPressed() {
+    // background(220);
+    // textSize(64); // font size
+    // //   text  x   y
+    // text(key, 50, 50);
+    // text(keyCode, 50, 120);
 
-//     // up arrow
-//     if (keyCode === 38) { 
-//         background(220);
-//         fill("red");
-//         circle(150,150,100);
-//     }
+    // up arrow
+    if (keyCode === 38) { 
+        background(220);
+        fill("red");
+        circle(150,150,100);
+    }
 
-//     // down arrow
-//     if (keyCode === 40) { 
-//         background(220);
-//         fill("black");
-//         circle(150,150,100);
-//     }
-// }
+    // down arrow
+    if (keyCode === 40) { 
+        background(220);
+        fill("black");
+        circle(150,150,100);
+    }
+}
 
-// function keyReleased() {
-//     rectSize = 50;
-// }
+function keyReleased() {
+    rectSize = 50;
+}
