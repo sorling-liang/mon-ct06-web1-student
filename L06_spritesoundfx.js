1998d2f10657298b3494cf7725127b720f126756
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-11 10:51:35 +0000

    Aug 11, 2025, 10:51 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -8,7 +8,7 @@ function preload() {
 // run once
 function setup() {
     createCanvas(600, 400);
-    background(80); // valid value 0-255
+    background(220); // valid value 0-255
 }
 
 // repeatedly 60 times per second
