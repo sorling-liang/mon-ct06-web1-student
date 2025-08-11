2136d17d1ad79dda127fba7dd9f0c8240d565893
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-11 13:14:38 +0000

    Aug 11, 2025, 1:14 PM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -13,24 +13,15 @@ function draw() {
 }
 
 function keyPressed() {
-    // background(220);
-    // textSize(64); // font size
-    // //   text  x   y
-    // text(key, 50, 50);
-    // text(keyCode, 50, 120);
 
     // up arrow
     if (keyCode === 38) { 
-        background(220);
-        fill("red");
-        circle(150,150,100);
+ 
     }
 
     // down arrow
     if (keyCode === 40) { 
-        background(220);
-        fill("black");
-        circle(150,150,100);
+
     }
 }
 
