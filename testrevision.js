f3665c4077007309e182e55b00d69a7f8a3942e1
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-08-18 09:54:10 +0000

    Aug 18, 2025, 9:54 AM

--- testrevision.js
+++ testrevision.js
@@ -6,9 +6,10 @@ function setup() {
 }
 
 function draw() {
+    fill('red');
     // 3 vertices
     // x1, y1,
     // x2, y2,
     // x3, y3
-    triangle(350,300, 450,300, 400,200);
+    triangle()
 }
