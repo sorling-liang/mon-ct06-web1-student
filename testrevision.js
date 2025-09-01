cbe280b07667de8a6f6495f267578e8e12af1004
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-01 09:21:21 +0000

    Sep 1, 2025, 9:21 AM

--- testrevision.js
+++ testrevision.js
@@ -1,28 +1,9 @@
-// test preparation
-let x = 100;
-let y = 100;
-let w = 30;
-let h = 30;
-let gray = 0;
+
 
 function setup() {
-    createCanvas(800, 600);
-    background("steelblue");
-    noLoop();
+
 }
 
 function draw() {
-    
-    // 3 vertices
-    // x1, y1,
-    // x2, y2,
-    // x3, y3
-    // triangle(350,300, 450,300, 400,200);
 
-    for (let count=0; count<5; count++) {
-        fill(gray);
-        rect(x, y, w, h);
-        x = x+ 40; // keep changing the xpos
-        gray = gray + 50;
-    }
 }
