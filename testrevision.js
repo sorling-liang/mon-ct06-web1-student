14904ec729391d63c3afddb8efafeb6b5cfc55f9
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-01 09:43:17 +0000

    Sep 1, 2025, 9:43 AM

--- testrevision.js
+++ testrevision.js
@@ -15,6 +15,6 @@ function draw() {
         circle(xpos, ypos, 50);
         xpos = xpos+50;
         ypos = ypos+50;
-        colorValue = colorValue + 50;
+        colorValue = colorValue + 15;
     }
 }
