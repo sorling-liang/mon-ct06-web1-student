ebd741514280f4c948c22c4c5ea7299859d5fbe1
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-01 09:40:46 +0000

    Sep 1, 2025, 9:40 AM

--- testrevision.js
+++ testrevision.js
@@ -6,14 +6,5 @@ function setup() {
 }
 
 function draw() {
-    fill("black");
-    stroke("red");
-    strokeWeight(10);
-    let middleX = width/2;
-    let middleY = height/2;
-
-    circle(middleX-150, middleY, 100);
-    rect(middleX-50+150, middleY-50, 100, 100);
-
-    triangle(240,middleY+50,   middleX,150,   350, middleY+50)
+    circle(50,50,50);
 }
