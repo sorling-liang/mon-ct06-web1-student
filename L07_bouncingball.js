8da2d243302d9dfe3f6327aac8c34e3f15190786
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-08 09:39:04 +0000

    Sep 8, 2025, 9:39 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -12,21 +12,21 @@ function setup() {
 // repeatedly 60 times per second
 function draw() {
     background(80);
-    circle(x, height/2, 50);
+    circle(x, height/2, ballSize);
 
-    if (keyIsDown(LEFT_ARROW)) {
-        x = x -5;
-    }
-    // will not go into the left wall
-    if (x < 25) {
-        x = 25;
-    }
-    // right arrow
-    if (keyIsDown(RIGHT_ARROW)) {
-        x = x +5;
-    }
-    // keep the ball from going into right wall
-    if (x > 400-25) {
-        x = 400-25;
-    }
+    // if (keyIsDown(LEFT_ARROW)) {
+    //     x = x -5;
+    // }
+    // // will not go into the left wall
+    // if (x < 25) {
+    //     x = 25;
+    // }
+    // // right arrow
+    // if (keyIsDown(RIGHT_ARROW)) {
+    //     x = x +5;
+    // }
+    // // keep the ball from going into right wall
+    // if (x > 400-25) {
+    //     x = 400-25;
+    // }
 }
