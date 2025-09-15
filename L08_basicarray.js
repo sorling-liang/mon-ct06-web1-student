e9ee5ab7937eb3863a1ea0f19a48e9b01cce621e
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-15 09:50:04 +0000

    Sep 15, 2025, 9:50 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -9,7 +9,7 @@ function setup() {
     createCanvas(400, 400);
     background(200);
     fill("orange");
-    rect( width/2, height/2, 200, 25);
+    rect( width/2, height/2, 200, 200);
 
     fill("black")
     textSize(32);
