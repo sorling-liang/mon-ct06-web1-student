ca6cb4d93ec845ef276878958d61bd158e22e5ca
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-15 10:58:13 +0000

    Sep 15, 2025, 10:58 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -3,7 +3,7 @@
 // run once
 function setup() {
     createCanvas(600, 400);
-    background(80); // valid value 0-255
+    background("black"); // valid value 0-255
 }
 
 // repeatedly 60 times per second
