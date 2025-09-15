d0046d10d13dc6303f7972124190a0a839ab64a8
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-15 09:42:54 +0000

    Sep 15, 2025, 9:42 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -5,27 +5,31 @@ function preload() {
     soundsfx = loadSound('assets/star_wars_theme_8_bit.mp3');
 }
 
-// run once
 function setup() {
-    createCanvas(800, 400);
-    background(80); // valid value 0-255
+    ca
+}
 
-    fill("cyan");
-    textSize(32);
-    //text("Hello my friend!", 20, 50);
-    //text("Today is Monday!", 20, 90);
+// run once
+// function setup() {
+//     createCanvas(800, 400);
+//     background(80); // valid value 0-255
 
-    let ypos = 50;
-    let favFoods = ["pizza", "chicken wings", "bubble tea", "ice cream", "chocolates"];
-    for (let index=0; index < favFoods.length; index++ ) {
-        // python print()
-        console.log( favFoods[index] );
-        //                      x    y
-        text( favFoods[index], 20, ypos);
-        ypos = ypos + 50; // go down to next line
-    }
+//     fill("cyan");
+//     textSize(32);
+//     //text("Hello my friend!", 20, 50);
+//     //text("Today is Monday!", 20, 90);
 
-} // end of setup()
+//     let ypos = 50;
+//     let favFoods = ["pizza", "chicken wings", "bubble tea", "ice cream", "chocolates"];
+//     for (let index=0; index < favFoods.length; index++ ) {
+//         // python print()
+//         console.log( favFoods[index] );
+//         //                      x    y
+//         text( favFoods[index], 20, ypos);
+//         ypos = ypos + 50; // go down to next line
+//     }
+
+// } // end of setup()
 
 
 // repeatedly 60 times per second
