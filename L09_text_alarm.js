ac501177f973b9ef153afba41b6b4e2400a73579
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-22 10:46:52 +0000

    Sep 22, 2025, 10:46 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -39,7 +39,7 @@ function mousePressed() {
 
 function keyPressed() {
     if (keyCode === 32) {
-        bgcolor = color( random(255), r)
+        bgcolor = color( random(255), random(255), random(255) );
     }
 }
 
