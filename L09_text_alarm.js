2443f4fca3f4e61cb7648800f6cbbbec3c0c771b
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-22 10:11:21 +0000

    Sep 22, 2025, 10:11 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -12,7 +12,7 @@ function setup() {
 function draw() {
     background("pink"); // valid value 0-255
 
-    let hour24 = hour();
+    let hour24 = (hour() + 1)/2;
     let nowMinute = minute();
     textSize(32);
     textAlign(CENTER, CENTER);
