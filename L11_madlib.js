9871d47579781a09d99bf5ec8a81b499126566bc
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-22 09:28:06 +0000

    Sep 22, 2025, 9:28 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -1,1 +1,12 @@
 // write your codes here
+
+// run once
+function setup() {
+    createCanvas(600, 400);
+    background("black"); // valid value 0-255
+}
+
+// repeatedly 60 times per second
+function draw() {
+
+}
