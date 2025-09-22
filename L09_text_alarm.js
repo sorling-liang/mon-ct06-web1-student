0e08635e296bc70f1128d72e28e5d78b276ab212
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-09-22 10:04:07 +0000

    Sep 22, 2025, 10:04 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -9,19 +9,24 @@ function setup() {
     xpos = width/2;
 }
 
-// repeatedly 60 times per second
 function draw() {
-    background("pink"); // valid value 0-255
-    textSize(32);
-    textAlign(CENTER, CENTER);
-    fill("purple");
-    text("Bounce!", xpos, height/2);
-    xpos = xpos + velocityX;
 
-    if (xpos > width-60) {
-        velocityX = velocityX * -1;
-    }
-    else if (xpos < 0+60) {
-        velocityX = velocityX * -1;
-    }
 }
+
+
+// repeatedly 60 times per second
+// function draw() {
+//     background("pink"); // valid value 0-255
+//     textSize(32);
+//     textAlign(CENTER, CENTER);
+//     fill("purple");
+//     text("Bounce!", xpos, height/2);
+//     xpos = xpos + velocityX;
+
+//     if (xpos > width-60) {
+//         velocityX = velocityX * -1;
+//     }
+//     else if (xpos < 0+60) {
+//         velocityX = velocityX * -1;
+//     }
+// }
