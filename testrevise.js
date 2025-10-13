25387502ca47b19a2bfe4ba2dd0a12cec49607be
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-10-13 10:40:08 +0000

    Oct 13, 2025, 10:40 AM

--- testrevise.js
+++ testrevise.js
@@ -1,24 +1,25 @@
 /*
 set the color of the background
-*/
-let xpos = 50;
-let velocity = 2;
+// */
 
-function setup() {
-    createCanvas(800,600);
-    background("purple");
-}
+// let xpos = 50;
+// let velocity = 2;
 
-function draw() {
-    background("purple");
-    circle(xpos, 50,50);
-    xpos = xpos + velocity;
+// function setup() {
+//     createCanvas(800,600);
+//     background("purple");
+// }
 
-    if (xpos > width) {
-        velocity = velocity * -1;
-    }
-    if (xpos < 0) {
-        velocity = velocity * -1;
-    }
+// function draw() {
+//     background("purple");
+//     circle(xpos, 50,50);
+//     xpos = xpos + velocity;
+
+//     if (xpos > width) {
+//         velocity = velocity * -1;
+//     }
+//     if (xpos < 0) {
+//         velocity = velocity * -1;
+//     }
     
-}
+// }
