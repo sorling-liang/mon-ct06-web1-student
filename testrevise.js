17bbe9dff6416d9af00f6cf05150d6c9ad9594d5
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-10-13 10:36:16 +0000

    Oct 13, 2025, 10:36 AM

--- testrevise.js
+++ testrevise.js
@@ -1,6 +1,7 @@
 /*
 set the color of the background
 */
+let xpos = 50;
 
 function setup() {
     createCanvas(400,600);
@@ -8,5 +9,6 @@ function setup() {
 }
 
 function draw() {
-    circle(xpos, 50, )
+    circle(xpos, 50,50);
+    xpos = xpos + 5;
 }
