bdf46837459434732df77b6ea72d1638f5f24d84
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-10-13 10:29:09 +0000

    Oct 13, 2025, 10:29 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -47,6 +47,6 @@ function draw() {
     textAlign(RIGHT, CENTER);
     text("Enter noun:", 180,60);
     text("Enter verb:", 180,110);
-    text("Enter adjective:", 180,180);
+    text("Enter adjective:", 180,170);
 
 }
