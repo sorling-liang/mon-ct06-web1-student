6f95b9510ff456b86d32134ff837dd774d460a81
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-11-03 10:11:20 +0000

    Nov 3, 2025, 10:11 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -1,1 +1,23 @@
 // write your codes here
+
+let hiddenwords = ["beach", "cloud", "mango"];
+
+let textInput;
+let guessButton;
+
+function setup() {
+    createCanvas(600,600);
+    background("lightblue");
+    textInput = createInput();
+    textInput.position(160,240);
+    guessButton = createButton("Guess");
+    guessButton.position(340,240);
+}
+
+function draw() {
+    textAlign(CENTER, CENTER);
+    textSize(30);
+    text("Guess the hidden 5-letter word!", width/2, 80);
+    text("Attempts: 0", width/2, 120);
+    text("Hints: S _ _ _ _", width/2, 160);
+}
